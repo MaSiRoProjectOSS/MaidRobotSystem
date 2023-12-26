c188627ab2fb2e0cf95f81bd724184a7e2e70de9
ciro-hu <masiro.to.akari@gmail.com>
2023-12-27 06:20:37 +0900

    Add Python requirements installation and cv-bridge to installation scripts

--- src/planetary_module/ros/script/install/01_head_unit/file/_01_02_install_python.sh
+++ src/planetary_module/ros/script/install/01_head_unit/file/_01_02_install_python.sh
@@ -3,6 +3,7 @@
 echo "==================================="
 echo "01_02_install_python.sh"
 echo "==================================="
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 ## update apt
 sudo apt update
@@ -11,7 +12,11 @@ sudo apt upgrade -y
 ## install python3-pip
 sudo apt install -y python3-pip
 sudo python3 -m pip install --upgrade pip
-sudo python3 -m pip install -r requirements.txt
+sudo python3 -m pip install -r $CURRENT_DIR/requirements.txt
+
+sudo apt install -y python3-cv-bridge
+
+
 
 ## Jetson health monitoring
 #sudo -H pip install jetson-stats
