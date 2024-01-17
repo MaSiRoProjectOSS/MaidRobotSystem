99dcc0cbd5c0871f4438fdfe8b2ebde0fd74c035
Akari <masiro.to.akari@gmail.com>
2024-01-17 22:53:48 +0900

    Add error display function to main.cpp

--- src/satellite_module/unit/head_unit/neck_controller/src/main.cpp
+++ src/satellite_module/unit/head_unit/neck_controller/src/main.cpp
@@ -35,6 +35,15 @@ public:
 
 HardwareSerialEX SerialEX(0);
 NeckController modbus(&SerialEX);
+void display_error(void)
+{
+    M5.Lcd.clear((uint16_t)BLACK);
+    M5.Lcd.setTextColor(RED, BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.setRotation(2);
+    M5.Lcd.printf("%s\r\n version: %s\r\n\r\n", APP_NAME, APP_VERSION);
+    M5.Lcd.printf("modbus begin failed\r\n");
+}
 
 void setup(void)
 {
@@ -43,14 +52,29 @@ void setup(void)
     delay(1000);
     bool result = modbus.begin(MODBUS_ADDRESS, (MessageFrame::MODBUS_TYPE)CUSTOM_MODBUS_TYPE);
     if (false == result) {
-        M5.Lcd.clear((uint16_t)BLACK);
-        M5.Lcd.setTextColor(RED, BLACK);
-        M5.Lcd.setCursor(0, 0);
-        M5.Lcd.setRotation(2);
-        M5.Lcd.printf("%s\r\n version: %s\r\n\r\n", APP_NAME, APP_VERSION);
-        M5.Lcd.printf("modbus begin failed\r\n");
+        int count_down = (1000 * 10) / LOOP_SPAN;
+        bool flag_wake = true;
+        display_error();
         while (false == result) {
-            delay(1000);
+            M5.update();
+            if (M5.BtnA.wasPressed()) {
+                count_down = (1000 * 10) / LOOP_SPAN;
+                M5.Lcd.wakeup();
+                display_error();
+                flag_wake = true;
+            }
+            if (true == flag_wake) {
+                if (0 < count_down) {
+                    count_down--;
+                    if (0 == count_down) {
+                        count_down = 0;
+                        M5.Lcd.clear();
+                        M5.Lcd.sleep();
+                        flag_wake = false;
+                    }
+                }
+            }
+            delay(LOOP_SPAN);
         }
     }
     M5.Lcd.sleep();
@@ -71,7 +95,8 @@ void update_imu(void)
 void loop(void)
 {
     static int count      = 0;
-    static int count_down = 0;
+    static int count_down = (1000 * 10) / LOOP_SPAN;
+    static bool flag_wake = true;
 
     M5.update();
 
@@ -83,31 +108,33 @@ void loop(void)
     if (M5.BtnA.wasPressed()) {
         count_down = (1000 * 10) / LOOP_SPAN;
         M5.Lcd.wakeup();
+        flag_wake = true;
     }
+    if (true == flag_wake) {
+        if (0 < count_down) {
+            count_down--;
+            if (0 == (count_down % 10)) {
+                UBaseType_t stack_cushy = SerialEX.get_stack_high_water_mark();
+                UBaseType_t max_cushy   = SerialEX.get_stack_size();
 
-    if (0 < count_down) {
-        count_down--;
-        if (0 == (count_down % 10)) {
-            UBaseType_t stack_cushy = SerialEX.get_stack_high_water_mark();
-            UBaseType_t max_cushy   = SerialEX.get_stack_size();
+                M5.Lcd.setCursor(0, 0);
+                M5.Lcd.setRotation(2);
+                M5.Lcd.printf("%s\r\n version: %s\r\n\r\n", APP_NAME, APP_VERSION);
+                M5.Lcd.printf("Accel:\r\n %6d,%6d,%6d\r\n", (int)modbus.accel.x, (int)modbus.accel.y, (int)modbus.accel.z);
+                M5.Lcd.printf("Gyro:\r\n %6d,%6d,%6d\r\n", (int)(modbus.gyro.x), (int)(modbus.gyro.y), (int)(modbus.gyro.z));
 
-            M5.Lcd.setCursor(0, 0);
-            M5.Lcd.setRotation(2);
-            M5.Lcd.printf("%s\r\n version: %s\r\n\r\n", APP_NAME, APP_VERSION);
-            M5.Lcd.printf("Accel:\r\n %6d,%6d,%6d\r\n", (int)modbus.accel.x, (int)modbus.accel.y, (int)modbus.accel.z);
-            M5.Lcd.printf("Gyro:\r\n %6d,%6d,%6d\r\n", (int)(modbus.gyro.x), (int)(modbus.gyro.y), (int)(modbus.gyro.z));
-
-            M5.Lcd.printf("\r\nCount [%6d/%6d]\r\n", modbus.count_process, modbus.count_reception);
-            M5.Lcd.printf("STACK [%6d/%6d]\r\n", (int)(stack_cushy), (int)max_cushy);
-            M5.Lcd.printf("OSC-FREQ [%10d]\r\n", modbus.get_oscillator_frequency());
-            M5.Lcd.printf("PWM-FREQ [%10d]\r\n", modbus.get_pwm_freq());
-        }
-        if (0 == count_down) {
-            count_down = 0;
-            M5.Lcd.clear();
-            M5.Lcd.sleep();
+                M5.Lcd.printf("\r\nCount [%6d/%6d]\r\n", modbus.count_process, modbus.count_reception);
+                M5.Lcd.printf("STACK [%6d/%6d]\r\n", (int)(stack_cushy), (int)max_cushy);
+                M5.Lcd.printf("OSC-FREQ [%10d]\r\n", modbus.get_oscillator_frequency());
+                M5.Lcd.printf("PWM-FREQ [%10d]\r\n", modbus.get_pwm_freq());
+            }
+            if (0 == count_down) {
+                count_down = 0;
+                M5.Lcd.clear();
+                M5.Lcd.sleep();
+                flag_wake = false;
+            }
         }
     }
-
     delay(LOOP_SPAN);
 }
